9eec064262b81f9d04eee26625f91e47ce837387
Chris MacDonald <cmacdonald@pbs.org>
2020-09-29 16:31:19 -0700

    2015 day 4 part 2 and refactor

--- 2015/4/implementation.go
+++ 2015/4/implementation.go
@@ -3,12 +3,20 @@ package main
 import (
 	"crypto/md5"
 	"fmt"
+	"hash"
 	"io"
 	"strconv"
 	"strings"
 )
 
-func partOne(key string) int {
+// Verifier checks a hash against some criteria
+type Verifier func(h hash.Hash) bool
+
+func hashToString(h hash.Hash) string {
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func mine(key string, verifier Verifier) int {
 	candidate := 0
 	for {
 		var combinedString strings.Builder
@@ -16,14 +24,37 @@ func partOne(key string) int {
 		combinedString.WriteString(strconv.Itoa(candidate))
 		hash := md5.New()
 		io.WriteString(hash, combinedString.String())
-		if string([]rune(fmt.Sprintf("%x", hash.Sum(nil)))[0:5]) == "00000" {
+		if verifier(hash) {
 			return candidate
 		}
 		candidate = candidate + 1
-
 	}
 }
 
+func partOne(key string) int {
+	verifier := func(h hash.Hash) bool {
+		for _, char := range []rune(hashToString(h))[0:5] {
+			if char != '0' {
+				return false
+			}
+		}
+		return true
+	}
+	return mine(key, verifier)
+}
+
+func partTwo(key string) int {
+	verifier := func(h hash.Hash) bool {
+		for _, char := range []rune(hashToString(h))[0:6] {
+			if char != '0' {
+				return false
+			}
+		}
+		return true
+	}
+	return mine(key, verifier)
+}
+
 func main() {
-	fmt.Println(partOne("yzbqklnj"))
+	fmt.Println(partTwo("yzbqklnj"))
 }
